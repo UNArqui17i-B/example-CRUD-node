95fee18910ca52501faa64e544ce7d2972b7def7
maasencioh <maasencioh@gmail.com>
2017-03-03 19:25:39 -0500

    feat: MongoDB CRUD

--- src/resources/books.js
+++ src/resources/books.js
@@ -3,13 +3,62 @@
 const express = require('express');
 const status = require('http-status');
 
-module.exports = function () {
+module.exports = function (Book) {
     const router = express.Router();
 
     // list of all books
     router.get('/', function (req, res) {
-        res.status(status.OK).send({
-            working: true
+        Book.find().exec(function (err, data) {
+            if (err) {
+                res.status(status.INTERNAL_SERVER_ERROR).send(err);
+            } else {
+                res.status(status.OK).send(data);
+            }
+        });
+    });
+
+    // search for id
+    router.get('/:id', function (req, res) {
+        Book.findOne({id: req.params.id}, function (err, data) {
+            if (err) {
+                res.status(status.BAD_REQUEST).send(err);
+            } else {
+                res.status(status.OK).send(data);
+            }
+        });
+    });
+
+    // create a book
+    router.post('/', function (req, res) {
+        let book = new Book(req.body);
+        book.save(function (err, data) {
+            if (err) {
+                res.status(status.INTERNAL_SERVER_ERROR).send(err);
+            } else {
+                res.status(status.CREATED).send(data);
+            }
+        });
+    });
+
+    // update a book
+    router.put('/:id', function (req, res) {
+        Book.update({id: req.params.id}, req.body, function (err, data) {
+            if (err) {
+                res.status(status.BAD_REQUEST).send(err);
+            } else {
+                res.status(status.OK).send(data);
+            }
+        });
+    });
+
+    // delete a book
+    router.delete('/:id', function (req, res) {
+        Book.find({id: req.params.id}).remove().exec(function (err, data) {
+            if (err) {
+                res.status(status.INTERNAL_SERVER_ERROR).send(err);
+            } else {
+                res.status(status.OK).send(data);
+            }
         });
     });
 
